6bb1018c0c8a718aeef8c595713eeb3875c1ffcd
Rick Leos <richardleos@outlook.com>
2020-12-28 17:22:49 -0800

    all data lookups made

--- Get-Ipdetails.ps1
+++ Get-Ipdetails.ps1
@@ -7,31 +7,46 @@ function Get-Ipdetails {
             Mandatory=$true)]
             [string[]]$CSVPath,
           [Parameter(
-            Position=0,
+            Position=1,
             HelpMessage="The Column Name that contain the IPs for the detailed up.",
             ValueFromPipelineByPropertyName=$true,
             Mandatory=$true)]
             [string[]]$IPColumnName,
         [Parameter(
-            Position=0,
+            Position=2,
             HelpMessage="Use to cobine ip details with the data of the CSV file.",
             ValueFromPipelineByPropertyName=$true)]
-            [switch[]]$CombineResults,
+            [switch]$CombineResults,
         [Parameter(
-            Position=0,
+            Position=3,
             HelpMessage="Use to cobine ip details with the data of the CSV file.",
             ValueFromPipelineByPropertyName=$true)]
             [string[]]$SecondaryDNSLookupServer,
         [Parameter(
-            Position=0,
+            Position=4,
             HelpMessage="Use to cobine ip details with the data of the CSV file.",
             ValueFromPipelineByPropertyName=$true)]
-            [switch[]]$ADcomputerLookup
+            [switch]$ADcomputerLookup,
+        [Parameter(
+            Position=5,
+            HelpMessage="Use to cobine ip details with the data of the CSV file.",
+            ValueFromPipelineByPropertyName=$true)]
+            [string[]]$ADServer
+    
     )
     $file = Import-Csv $CSVPath
+    $collection = @()
+    foreach ($lookupitem in $file) {
+        $IP = $lookupitem.$IPColumnName
+        $lookup = Resolve-DnsName $IP
+        if ($SecondaryDNSLookupServer) {
+            $secondarylookup = Resolve-DnsName $lookupitem.$IPColumnName -Server $SecondaryDNSLookupServer
+        }
+        if ($ADcomputerLookup) {
+            Import-Module ActiveDirectory
+            $adlookup = Get-ADComputer -Properties IPv4Address -Filter {IPv4Address -eq $IP}
+        }
 
+    }
 
 }
-
-Get-Ipdetails
-Import-Csv
